863af7a9c4119d0438c5688877524967905511ce
Oğuz Kağan DÖNMEZ <98166178+okadonmez@users.noreply.github.com>
2024-04-03 21:03:22 +0300

    Revert "11 Yeni commit eklendi."

--- SentimentAnalysis/app/src/main/java/com/example/sentimentanalysis/MainActivity.java
+++ SentimentAnalysis/app/src/main/java/com/example/sentimentanalysis/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.sentimentanalysis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        init();
-    }
-
-    private void init(){
-        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-        startActivity(intent);
     }
 }
