fa9f9afa60f9fe0be0f780ef293013284c8e9b62
Oguz Kagan Donmez <oguzkagandonmez01@gmail.com>
2024-05-07 13:24:11 +0300

    AndroidManifest.xml dosyasina internet izni eklendi. Begenme sistemi icin drawable klasorune icon_empty_heart.png ve icon_full_heart.png gorselleri eklendi. SuggestionPageMode.java sinifina setter methodlari eklendi.

--- SentimentAnalysis/app/src/main/java/com/example/sentimentanalysis/Analyzing/SuggestionPage/SuggestionPageModel.java
+++ SentimentAnalysis/app/src/main/java/com/example/sentimentanalysis/Analyzing/SuggestionPage/SuggestionPageModel.java
@@ -15,11 +15,23 @@ public class SuggestionPageModel {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public int getImage() {
         return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
     }
 }
